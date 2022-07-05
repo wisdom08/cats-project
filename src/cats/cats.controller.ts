de6cd8f0b14244bdeb5623ecd383d7185c32e171
wisdom <wisdom08@kakao.com>
2022-07-05 17:31:09 +0900

    feat: cats controller 설계

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,37 @@
+import { UseFilters, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getCurrentCat() {
+    return 'current cat';
+  }
+
+  @Post()
+  async signUp() {
+    return 'signup';
+  }
+
+  @Post('login')
+  logIn() {
+    return 'login';
+  }
+
+  @Post('logout')
+  logOut() {
+    return 'logout';
+  }
+
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
+  }
+}
