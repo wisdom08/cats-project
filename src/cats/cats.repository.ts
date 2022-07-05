44483a9fdd7d88e647b5a2839b3e17f417fd115e
wisdom <wisdom08@kakao.com>
2022-07-05 22:05:06 +0900

    feat: Repository 패턴 적용

---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat } from './cats.schema';
+import { Model } from 'mongoose';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsByEmail(email: string): Promise<boolean> {
+    return await this.catModel.exists({ email });
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,23 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CatRequestDto } from './dto/cats.request.dto';
-import { Cat } from './cats.schema';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
   async singUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existsByEmail(email);
     if (isCatExist) {
       throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
     }
 
     const saltOrRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltOrRounds);
-    const savedCat = await this.catModel.create({
+    const savedCat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
