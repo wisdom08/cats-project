6e9f020f2be5653a38872d7de665d0e2a7f426c1
wisdom <wisdom08@kakao.com>
2022-07-07 18:43:31 +0900

    feat: 인증 기능 보완

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Payload } from './jwt.payload';
+import { CatsRepository } from '../../cats/cats.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly catsRepository: CatsRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: 'secret',
       ignoreExpiration: false,
     });
+  }
+
+  async validate(payload: Payload) {
+    const cat = this.catsRepository.findCatByIdWithoutPassword(payload.sub);
+    if (cat) {
+      return cat;
+    } else {
+      throw new UnauthorizedException('접근 오류');
+    }
   }
 }
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Body, UseFilters, UseInterceptors } from '@nestjs/common';
+import { Body, UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
 import { Controller, Get, Post } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
@@ -8,6 +8,8 @@ import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ReadOnlyCatDto } from './dto/cat.dto';
 import { AuthService } from '../auth/auth.service';
 import { LoginRequestDto } from '../auth/dto/login.request.dto';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { CurrentUser } from '../common/decorators/user.decorator';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -19,9 +21,10 @@ export class CatsController {
   ) {}
 
   @ApiOperation({ summary: '현재 고양이 가져오기' })
+  @UseGuards(JwtAuthGuard)
   @Get()
-  getCurrentCat() {
-    return 'current cat';
+  getCurrentCat(@CurrentUser() cat) {
+    return cat.readOnlyData;
   }
 
   @ApiResponse({
